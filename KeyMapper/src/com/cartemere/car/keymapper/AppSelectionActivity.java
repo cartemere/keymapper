7612dcc9170d9b99428add10c01a9c17a22db20c
Cartemere <cartemere@online.fr>
2016-05-01 00:30:29 +0200

    [KM] use detached thread to load App list

--- KeyMapper/src/com/cartemere/car/keymapper/AppSelectionActivity.java
+++ KeyMapper/src/com/cartemere/car/keymapper/AppSelectionActivity.java
@@ -1,177 +1,48 @@
 package com.cartemere.car.keymapper;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.cartemere.car.keymapper.action.KeyMapperAction;
-import com.cartemere.car.keymapper.cache.AppAssociationCache;
 import com.cartemere.car.keymapper.model.AppAssociation;
+import com.cartemere.car.keymapper.task.AppListTask;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 public class AppSelectionActivity extends Activity {
 
-	public static final String PERSISTENCE_PACKAGE_PREFIX = "key_package_name_";
-	public static final String PERSISTENCE_ENABLE_PREFIX = "key_enable_";
-	private List<Map<String, String>> appPropertyList = null;
-	String iconKey = "icon";
-	String appName = "name";
-	String appPackage = "package";
-	boolean cancelAppListLoad;
 	private AppAssociation parentAssociation = null;
-	private Activity instance;
+	private AppListTask task;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		instance = this;
 		setContentView(R.layout.activity_app_selection);
-
 		getParentAssociation();
-		
-		cancelAppListLoad = false;
-		ListView appSelectionListView = (ListView) findViewById(R.id.list_select_app);
-		appPropertyList = new ArrayList<Map<String, String>>();
 
-		// retrieve list of installed apps
-		List<ApplicationInfo> appInfoList = null;
-		PackageManager pm = getPackageManager();
-		appInfoList = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+		ProgressDialog progressDialog = new ProgressDialog(this);
+		// detach thread to process Asynch loading...
+		task = new AppListTask(this, progressDialog);
 
 		// init progress bar
-		ProgressDialog progressDialog = new ProgressDialog(
-				AppSelectionActivity.this);
 		progressDialog.setTitle("Loading list of installed apps");
 		progressDialog.setCancelable(true);
 		progressDialog.setProgress(0);
-		progressDialog.setMax(appInfoList.size());
+		progressDialog.setMax(100);
 		progressDialog
 				.setOnCancelListener(new DialogInterface.OnCancelListener() {
 					@Override
 					public void onCancel(DialogInterface dialog) {
 						// click on Cancel Button
-						cancelAppListLoad = true;
+						task.cancel(true);
 					}
 				});
 		progressDialog.show();
 
-		// process elements
-		Map<String, String> propertyToValue;
-		for (ApplicationInfo appInfo : appInfoList) {
-			if (cancelAppListLoad) {
-				break;
-			}
-			// progressDialog.incrementProgressBy(1);
-			propertyToValue = new HashMap<String, String>();
-			propertyToValue.put(iconKey, String.valueOf(appInfo.icon));
-			propertyToValue.put(appName, appInfo.loadLabel(pm).toString());
-			propertyToValue.put(appPackage, appInfo.packageName);
-			appPropertyList.add(propertyToValue);
-		}
-		
-		// reorder elements to display
-		sortAppPropertyList();
-
-		progressDialog.dismiss();
-		// Map structure to display
-		SimpleAdapter mSchedule = new SimpleAdapter(this.getBaseContext(),
-				appPropertyList, R.layout.app_selection_fragment, new String[] {
-						iconKey, appName }, new int[] {
-						R.id.app_selection_img, R.id.app_selection_name });
-
-		appSelectionListView.setAdapter(mSchedule);
-
-		appSelectionListView
-				.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-					@Override
-					public void onItemClick(AdapterView<?> parent, View view,
-							int position, long id) {
-						final Map<String, String> propertyMap = appPropertyList
-								.get(position);
-						AlertDialog.Builder adb = new AlertDialog.Builder(
-								AppSelectionActivity.this);
-						
-						adb.setTitle("App selection for action " + parentAssociation.getKeyName());
-						String message = "App : " + propertyMap.get(appName)
-								+ "\nPackage : " + propertyMap.get(appPackage);
-						if (parentAssociation.getAppName() != null) {
-							message +=  "\n" + "\nPrevious App : " + parentAssociation.getAppName();
-						}
-						adb.setMessage(message);
-						adb.setPositiveButton("Set & Launch",
-								new DialogInterface.OnClickListener() {
-
-									@Override
-									public void onClick(DialogInterface dialog,
-											int which) {
-										parentAssociation.setAppName(propertyMap.get(appName));
-										parentAssociation.setAppPackageName(propertyMap.get(appPackage));
-								        AppAssociationCache cache = AppAssociationCache.getInstance(getApplicationContext());
-								        cache.updateAssociationByKey(getApplicationContext(), parentAssociation);
-								        // launch selected app
-										KeyMapperAction.launchSelectedApp(instance, propertyMap.get(appPackage));
-									}
-								});
-						adb.setNeutralButton("Set",
-								new DialogInterface.OnClickListener() {
-
-									@Override
-									public void onClick(DialogInterface dialog,
-											int which) {
-										parentAssociation.setAppName(propertyMap.get(appName));
-										parentAssociation.setAppPackageName(propertyMap.get(appPackage));
-										AppAssociationCache cache = AppAssociationCache.getInstance(getApplicationContext());
-								        cache.updateAssociationByKey(getApplicationContext(), parentAssociation);
-								        // close current activity (come back to main activity)
-								        instance.finish();
-									}
-								});
-						adb.setNegativeButton("Cancel", null);
-						adb.show();
-					}
-				});
-	}
-	
-	private void sortAppPropertyList() {
-		Comparator<Map<String, String>> comparator = new Comparator<Map<String, String>>() {
-
-			@Override
-			public int compare(Map<String, String> lhs, Map<String, String> rhs) {
-				if (lhs == null) {
-					return 0;
-				} else if (rhs == null) {
-					return 1;
-				} else {
-					String lhName = lhs.get(appName);
-					String rhName = rhs.get(appName);
-					if (lhName == null) {
-						return 0;
-					} else if (rhName == null) {
-						return 1;
-					} else {
-						return lhName.compareToIgnoreCase(rhName);
-					}
-				}
-			}
-		};
-		Collections.sort(appPropertyList, comparator);
+		// run async task
+		task.execute();
 	}
 
 	@Override
@@ -182,7 +53,7 @@ public class AppSelectionActivity extends Activity {
 				"Select app for key : " + parentAssociation.getKeyName());
 	}
 
-	public void getParentAssociation() {
+	public AppAssociation getParentAssociation() {
 		if (parentAssociation == null) {
 			Intent intent = getIntent();
 			if (intent != null) {
@@ -193,6 +64,7 @@ public class AppSelectionActivity extends Activity {
 				}
 			}
 		}
+		return parentAssociation;
 	}
 
 	@Override
---
+++ KeyMapper/src/com/cartemere/car/keymapper/task/AppListTask.java
@@ -0,0 +1,257 @@
+package com.cartemere.car.keymapper.task;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.cartemere.car.keymapper.AppSelectionActivity;
+import com.cartemere.car.keymapper.R;
+import com.cartemere.car.keymapper.action.KeyMapperAction;
+import com.cartemere.car.keymapper.cache.AppAssociationCache;
+import com.cartemere.car.keymapper.model.AppAssociation;
+
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+public class AppListTask extends AsyncTask<Void, Integer, SimpleAdapter> {
+
+	public static final String LOG_KEY = "AppListTask";
+	private AppSelectionActivity activity;
+	private ProgressDialog dialog;
+
+	private List<Map<String, String>> appPropertyList = new ArrayList<Map<String, String>>();
+	public static String TAG_ICON_KEY = "icon";
+	public static String TAG_APP_NAME = "name";
+	public static String TAG_APP_PACKAGE = "package";
+	boolean cancelAppListLoad = false;
+	
+	private static int PROGRESS_GET_APP_LIST = 20;
+	private static int PROGRESS_PROCESS_APP = 1;
+	private static int PROGRESS_SORT_LIST = 10;
+	private static int PROGRESS_CREATE_ADAPTER = 20;
+	private static int PROGRESS_SET_ADAPTER = 20;
+	private static int PROGRESS_RENDER = 20;
+	
+	
+	private int progressCurrent = 0;
+	private int progressMaxValue = 0;
+
+	public AppListTask(AppSelectionActivity parentActivity,
+			ProgressDialog progressDialog) {
+		super();
+		// keep parent activity to access context & attach result
+		this.activity = parentActivity;
+		// keep progressDialog to update it...
+		this.dialog = progressDialog;
+	}
+
+	public void cancel() {
+		cancelAppListLoad = true;
+	}
+
+	@Override
+	protected SimpleAdapter doInBackground(Void... params) {
+		SimpleAdapter mSchedule = null;
+		try {
+			// retrieve list of installed apps
+			List<ApplicationInfo> appInfoList = null;
+			PackageManager pm = activity.getPackageManager();
+			appInfoList = pm
+					.getInstalledApplications(PackageManager.GET_META_DATA);
+			if (!cancelAppListLoad) {
+
+				// we have list of all items, can estimate progress bar behavior
+				computeProgressMaxValue(appInfoList);
+				publishProgress(PROGRESS_GET_APP_LIST);
+				// process elements
+				Map<String, String> propertyToValue;
+				for (ApplicationInfo appInfo : appInfoList) {
+					if (cancelAppListLoad) {
+						break;
+					}
+					// progressDialog.incrementProgressBy(1);
+					propertyToValue = new HashMap<String, String>();
+					propertyToValue.put(TAG_ICON_KEY,
+							String.valueOf(appInfo.icon));
+					propertyToValue.put(TAG_APP_NAME, appInfo.loadLabel(pm)
+							.toString());
+					propertyToValue.put(TAG_APP_PACKAGE, appInfo.packageName);
+					appPropertyList.add(propertyToValue);
+					publishProgress(PROGRESS_PROCESS_APP);
+				}
+
+				if (!cancelAppListLoad) {
+
+					// reorder elements to display
+					sortAppPropertyList();
+					publishProgress(PROGRESS_SORT_LIST);
+
+					if (!cancelAppListLoad) {
+						mSchedule = new SimpleAdapter(
+								activity.getBaseContext(), appPropertyList,
+								R.layout.app_selection_fragment, new String[] {
+										AppListTask.TAG_ICON_KEY,
+										AppListTask.TAG_APP_NAME }, new int[] {
+										R.id.app_selection_img,
+										R.id.app_selection_name });
+						publishProgress(PROGRESS_CREATE_ADAPTER);
+					}
+				}
+			}
+		} catch (Exception ex) {
+			dialog.dismiss();
+			Log.e(LOG_KEY, "unexpected error while processing", ex);
+		}
+		return mSchedule;
+	}
+
+
+	@Override
+	protected void onPostExecute(SimpleAdapter mSchedule) {
+
+		try {
+			// attach adapter to List
+			ListView appSelectionListView = (ListView) activity
+					.findViewById(R.id.list_select_app);
+			appSelectionListView.setAdapter(mSchedule);
+			publishProgress(PROGRESS_SET_ADAPTER);
+
+			// set entry behavior
+			appSelectionListView
+					.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+						@Override
+						public void onItemClick(AdapterView<?> parent,
+								View view, int position, long id) {
+							final Map<String, String> propertyMap = appPropertyList
+									.get(position);
+							AlertDialog.Builder adb = new AlertDialog.Builder(
+									activity);
+
+							AppAssociation parentAssociation = activity
+									.getParentAssociation();
+
+							adb.setTitle("App selection for action "
+									+ parentAssociation.getKeyName());
+							String message = "App : "
+									+ propertyMap.get(AppListTask.TAG_APP_NAME)
+									+ "\nPackage : "
+									+ propertyMap
+											.get(AppListTask.TAG_APP_PACKAGE);
+							if (activity.getParentAssociation().getAppName() != null) {
+								message += "\n" + "\nPrevious App : "
+										+ parentAssociation.getAppName();
+							}
+							adb.setMessage(message);
+							adb.setPositiveButton("Set & Launch",
+									new DialogInterface.OnClickListener() {
+
+										@Override
+										public void onClick(
+												DialogInterface dialog,
+												int which) {
+											AppAssociation parentAssociation = activity
+													.getParentAssociation();
+											parentAssociation.setAppName(propertyMap
+													.get(AppListTask.TAG_APP_NAME));
+											parentAssociation
+													.setAppPackageName(propertyMap
+															.get(AppListTask.TAG_APP_PACKAGE));
+											AppAssociationCache cache = AppAssociationCache.getInstance(activity
+													.getApplicationContext());
+											cache.updateAssociationByKey(
+													activity.getApplicationContext(),
+													parentAssociation);
+											// launch selected app
+											KeyMapperAction.launchSelectedApp(
+													activity,
+													propertyMap
+															.get(AppListTask.TAG_APP_PACKAGE));
+										}
+									});
+							adb.setNeutralButton("Set",
+									new DialogInterface.OnClickListener() {
+
+										@Override
+										public void onClick(
+												DialogInterface dialog,
+												int which) {
+											AppAssociation parentAssociation = activity
+													.getParentAssociation();
+											parentAssociation.setAppName(propertyMap
+													.get(AppListTask.TAG_APP_NAME));
+											parentAssociation
+													.setAppPackageName(propertyMap
+															.get(AppListTask.TAG_APP_PACKAGE));
+											AppAssociationCache cache = AppAssociationCache.getInstance(activity
+													.getApplicationContext());
+											cache.updateAssociationByKey(
+													activity.getApplicationContext(),
+													parentAssociation);
+											// close current activity (come back
+											// to main activity)
+											activity.finish();
+										}
+									});
+							adb.setNegativeButton("Cancel", null);
+							adb.show();
+						}
+					});
+		} finally {
+			// release
+			dialog.dismiss();
+		}
+	}
+
+	@Override
+	protected void onPreExecute() {
+	}
+
+	@Override
+	protected void onProgressUpdate(Integer... values) {
+		if (values.length > 0) {
+		progressCurrent += values[0];
+		dialog.setProgress(progressCurrent * 100 / progressMaxValue);
+		}
+	}
+	
+	private void computeProgressMaxValue(List<ApplicationInfo> appInfoList) {
+		progressMaxValue = PROGRESS_GET_APP_LIST + PROGRESS_SORT_LIST + PROGRESS_CREATE_ADAPTER + PROGRESS_SET_ADAPTER + PROGRESS_RENDER + (PROGRESS_PROCESS_APP * appInfoList.size());
+	}
+
+	private void sortAppPropertyList() {
+		Comparator<Map<String, String>> comparator = new Comparator<Map<String, String>>() {
+
+			@Override
+			public int compare(Map<String, String> lhs, Map<String, String> rhs) {
+				if (lhs == null) {
+					return 0;
+				} else if (rhs == null) {
+					return 1;
+				} else {
+					String lhName = lhs.get(TAG_APP_NAME);
+					String rhName = rhs.get(TAG_APP_NAME);
+					if (lhName == null) {
+						return 0;
+					} else if (rhName == null) {
+						return 1;
+					} else {
+						return lhName.compareToIgnoreCase(rhName);
+					}
+				}
+			}
+		};
+		Collections.sort(appPropertyList, comparator);
+	}
+}
